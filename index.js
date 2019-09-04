990c28b51c9483d9706d10792189d18df091b3c0
“DivaAudriMaharani” <divareyy@gmail.com>
2019-09-04 19:35:29 +0700

    port-env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 9000;
+const port = process.env.PORT|| 9000;
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/plain'});
