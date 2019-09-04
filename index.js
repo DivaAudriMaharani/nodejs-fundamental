aae6b931041b1277cbc78614775e59537c0063b9
“DivaAudriMaharani” <divareyy@gmail.com>
2019-09-04 22:29:42 +0700

    hellow

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const http = require('http');
+/* const http = require('http');
 const port = process.env.PORT|| 9000;
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/plain'});
     res.end('{ "success":true, "data":"Index", "message":"Hello World", "code":200}');
-}).listen(port, '127.0.0.1');
+}).listen(port, '127.0.0.1'); */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+    res.send('Hello Diva');
+})
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
